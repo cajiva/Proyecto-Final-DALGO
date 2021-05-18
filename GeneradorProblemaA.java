8e5c3c59be81cb7b8678b4feb4de62f77d86bc3a
Catalina Jimenez <c.jimenezv@uniandes.edu.co>
2021-05-18 17:39:25 -0500

    Generador Funcional, pruebas en formato de salidas(.out)

---
+++ GeneradorProblemaA.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GeneradorProblemaA {
+
+    static int n, r, m;
+
+    public void generar()
+    {
+        System.out.println(n+" "+r+" "+m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j== n-1){
+                    System.out.print(ThreadLocalRandom.current().nextInt(0,51)+"\r\n");
+                }
+                else
+                    System.out.print(ThreadLocalRandom.current().nextInt(0,51)+" ");       
+            }
+        }
+        System.out.println("0 0 0");
+    }
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+
+        n = 50;
+        r = 999;
+        m = 999;
+
+        GeneradorProblemaA generador = new GeneradorProblemaA();
+        generador.generar();
+    }
+}
+
